81e2989fd73b1a3d927b5204bd89135a4fbd34eb
SamNithish <mrtnvicky2020@gmail.com>
2023-11-08 22:37:00 +0530

    first commit

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,112 @@
+import react, { useState } from 'react';
+import './WeatherApp.css';
+import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import humidity_icon from '../Assets/humidity.png';
+import rain_icon from '../Assets/rain.png';
+import search_icon from '../Assets/search.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+
+const WeatherApp = () => {
+
+    let api_key = "42035888a3c3e2a7115ed8ffee758dc6";
+
+    const [wicon, setWicon] = useState(cloud_icon);
+
+    const search = async () => {
+        const element = document.getElementsByClassName("cityInput");
+    
+        if (element[0].value === "") {
+            return;
+        }
+    
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&APPID=${api_key}`;
+    
+        try {
+            const response = await fetch(url);
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+    
+            const data = await response.json();
+    
+            const humidity = document.getElementsByClassName("humidity-percent");
+            const wind = document.getElementsByClassName("wind-rate");
+            const temperature = document.getElementsByClassName("weather-temp");
+            const location = document.getElementsByClassName("weather-location");
+    
+            if (data) {
+                humidity[0].innerHTML = data.main?.humidity + " %";
+                wind[0].innerHTML = data.wind?.speed + " km/h";
+                temperature[0].innerHTML = data.main?.temp + " °C";
+                location[0].innerHTML = data.name;
+                
+                const iconCode = data.weather[0].icon;
+
+        if (iconCode === "01d" || iconCode === "01n") {
+            setWicon(clear_icon);
+        } else if (iconCode === "02d" || iconCode === "02n") {
+            setWicon(cloud_icon);
+        } else if (iconCode === "03d" || iconCode === "03n") {
+            setWicon(drizzle_icon);
+        } else if (iconCode === "04d" || iconCode === "04n") {
+            setWicon(drizzle_icon);
+        } else if (iconCode === "09d" || iconCode === "09n") {
+            setWicon(rain_icon);
+        } else if (iconCode === "10d" || iconCode === "10n") {
+            setWicon(rain_icon);
+        } else if (iconCode === "13d" || iconCode === "13n") {
+            setWicon(snow_icon);
+        } else {
+            setWicon(clear_icon);
+        }
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+
+       
+    };
+    
+    
+    return(
+        <div className='container'>
+            <div className='top-bar'>
+                <input type="text" className='cityInput' placeholder='Search your city'></input>
+                <div className='search-icon' onClick={()=>{search()}}>
+                    <img src={search_icon} alt=""></img>
+                </div>
+            </div>
+            <div className='weather-image'>
+                <img src={wicon} alt="" />
+            </div>
+            <div className='weather-temp'>24°C</div>
+            <div className='weather-location'>India</div>
+            <div className='data-container'>
+                <div className='element'>
+                    <img src={humidity_icon} alt=""  className='icon'/>
+                    <div className='data'>
+                        <div className='humidity-percent'>65%</div>
+                        <div className='text'>Humidity</div>
+                    </div>
+                </div>
+                <div className='element'>
+                    <img src={wind_icon} alt=""  className='icon'/>
+                    <div className='data'>
+                        <div className='wind-rate'>24%</div>
+                        <div className='text'>Wind Speed</div>
+                    </div>
+                </div>
+            </div>
+            <br></br>
+            <div className='developed-by'>
+            | Developed by Vigneshwaran |
+            </div>
+        </div>
+    )
+}
+
+export default WeatherApp;
